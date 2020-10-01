3e5b30dc70ab64983b31f4da2679f5ec09b8e4c2
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2020-10-01 15:47:37 -0300

    video 3.1

--- lib/components/localization.dart
+++ lib/components/localization.dart
@@ -1,9 +1,11 @@
 // localization e internationalization
 
+import 'package:bytebank/components/progress.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'container.dart';
+import 'error.dart';
 
 class LocalizationContainer extends BlocContainer {
   final Widget child;
@@ -41,5 +43,63 @@ class ViewI18N {
     // a aplicacao
 
     return values[_language];
+  }
+}
+
+
+@immutable
+abstract class I18NMessagesState {
+  const I18NMessagesState();
+}
+
+@immutable
+class LoadingI18NMessagesState extends I18NMessagesState {
+  const LoadingI18NMessagesState();
+}
+
+@immutable
+class InitI18NMessagesState extends I18NMessagesState {
+  const InitI18NMessagesState();
+}
+
+@immutable
+class LoadedI18NMessagesState extends I18NMessagesState {
+  final I18NMessages _messages;
+
+  const LoadedI18NMessagesState(this._messages);
+}
+
+class I18NMessages {
+  final Map<String, String> _messages;
+  I18NMessages(this._messages);
+
+  String get(String key) {
+    assert(key != null);
+    assert(_messages.containsKey(key));
+    return _messages[key];
+  }
+
+}
+
+@immutable
+class FatalErrorI18NMessagesState extends I18NMessagesState {
+  const FatalErrorI18NMessagesState();
+}
+
+
+class I18NLoadingView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<I18NMessagesCubit, I18NMessagesState>(
+      builder: (context, state) {
+        if(state is InitI18NMessagesState || state is LoadingI18NMessagesState) {
+          return ProgressView();
+        }
+        if(state is LoadedI18NMessagesState) {
+          return TELA;
+        }
+        return ErrorView("Erro buscando mensagens da tela");
+      },
+    );
   }
 }
